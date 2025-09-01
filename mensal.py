10091bb8f7120f626b1b9a02457ea2fe722eae4c
carlos <carlossaraiva546@gmail.com>
2025-09-01 16:34:34 -0300

    implemento salario mensal

---
+++ mensal.py
@@ -0,0 +1,6 @@
+valor_hora = float(input("insira quanto você por hora:"))
+horas = int(input("insira quantas horas você trabalha:"))
+
+salario_do_mes = (valor_hora * horas)
+
+print ("seu salario do mês é",salario_do_mes)
